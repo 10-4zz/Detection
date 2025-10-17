f442dc10786a07cb3977ec6d772dda774a59f9f6
10-4zz <zhuxiangyi@m.ldu.edu.cn>
2025-10-17 16:38:45 +0800

    base architecture

--- models/architectures/base_architecture.py
+++ models/architectures/base_architecture.py
@@ -5,6 +5,8 @@ from typing import Dict, Optional, Union, Tuple, List
 
 import torch.nn as nn
 
+from utils.logger import logger
+
 
 class BaseArchitecture(nn.Module):
     def __init__(
@@ -12,18 +14,21 @@ class BaseArchitecture(nn.Module):
             input_size: Optional[Union[int, Tuple[int], List[list]]] = None,
     ):
         super(BaseArchitecture, self).__init__()
+
         self.input_size = input_size
+        self.get_model_info()
 
     def forward(self, *args, **kwargs):
         raise NotImplementedError("Forward method not implemented.")
 
-    def get_model_info(self) -> Dict:
+    def get_model_info(self):
         """
         This method will provide the information about the architecture or model. for example, parameters
         FLOPs and so on.
-        :return:
-            model_info: str
         """
-        pass
+        logger.warning(
+            f"If you want to learn about the information about {self.__class__.__name__}, "
+            "please implement the method get_model_info in your class."
+        )
 
 

41873ed0f2aeec78b77d3cea9d3fe9a1505de247
10-4zz <zhuxiangyi@m.ldu.edu.cn>
2025-10-17 19:07:31 +0800

    base architecture

--- models/architectures/base_architecture.py
+++ models/architectures/base_architecture.py
@@ -16,20 +16,27 @@ class BaseArchitecture(nn.Module):
         super(BaseArchitecture, self).__init__()
 
         self.input_size = input_size
-        self.get_model_info()
+        self.show_model_info()
 
     def forward(self, *args, **kwargs):
         raise NotImplementedError("Forward method not implemented.")
 
-    def get_model_info(self):
+    def show_model_info(self):
         """
         This method will provide the information about the architecture or model. for example, parameters
         FLOPs and so on.
         """
         logger.warning(
-            f"If you want to learn about the information about {self.__class__.__name__}, "
+            f"If you want to learn about the information about {self.__class__.__name__}'s parameters, FLOPS eta, "
             "please implement the method get_model_info in your class."
         )
+
+    def info(self):
+        """
+        This method is used to show some information which need be shown in terminal.
+        :return:
+        """
+        pass
 
 
 
